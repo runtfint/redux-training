cb84c5b6691e616a64811b4ab512ebe602f7c456
Egor <runtfint.runtfint@mail.ru>
2023-04-28 17:58:19 +0500

    added useTypedSelector

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 
 const UserList: React.FC = () => {
-	const state = useSelector(state => state);
-	console.log(state);
+	const { users, loading, error } = useTypedSelector(state => state.user);
+	console.log(users, loading, error);
 	return (
 		<div>
 			
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -4,3 +4,5 @@ import { userReducer } from "./userReducer"
 export const rootReducer = combineReducers({
     user: userReducer,
 })
+
+export type RootState = ReturnType<typeof rootReducer>
